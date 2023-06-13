1761733cbd2ad44a94f78fd7a27ca4cfcd5a292b
ikrarkhaera <ikrarkhaeraa@gmail.com>
2023-06-13 19:11:57 +0700

    revisi minor

--- app/src/main/java/com/example/fragaria/ResultActivity.kt
+++ app/src/main/java/com/example/fragaria/ResultActivity.kt
@@ -4,10 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import com.bumptech.glide.Glide
 import com.example.fragaria.databinding.ActivityResultBinding
-import kotlin.math.max
 
 class ResultActivity : AppCompatActivity() {
 
